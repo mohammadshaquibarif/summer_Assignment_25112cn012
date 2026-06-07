02c2350fbad382635cc2daf616d56ce74c3e5ebb
mohammadshaquibarif <shaquibarif.19@gmail.com>
2026-06-07 22:34:03 +0530

    Added day 7 assignment

---
+++ DAY7/ques25.cpp
@@ -0,0 +1,21 @@
+//Write a program to recursive factorial
+#include <iostream>
+using namespace std;
+
+int factorial(int x)
+{
+    if (abs(x) < 2)
+        return 1;
+    return x * factorial(x - 1);
+}
+int main()
+{
+    int n;
+    cout << "enter the number whose factorial is to be calculated: ";
+    cin >> n;
+
+    int fact = factorial(n);
+
+    cout << fact << endl;
+    return 0;
+}
