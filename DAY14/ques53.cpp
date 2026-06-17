b5556f011bb3be8af48274107cad54fd35d50824
mohammadshaquibarif <shaquibarif.19@gmail.com>
2026-06-17 23:25:09 +0530

    Added day 14 assignment

---
+++ DAY14/ques53.cpp
@@ -0,0 +1,25 @@
+// write a program to linear search
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {34, 56, 73, 55, 60};
+
+    int target = 0;
+    int i;
+    int index = 0;
+    for (int &x : arr)
+    {
+        if (x == target)
+        {
+            cout << "the target index is: "  << index;
+            return 0;
+        }
+        index++;
+    }
+
+    cout << "the target is not in the array" << endl;
+
+    return 0;
+}
